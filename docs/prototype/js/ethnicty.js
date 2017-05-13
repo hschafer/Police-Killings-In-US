dc0834e9c07a6338251a1a177a2b1e0bdae26bb6
Hunter Schafer <hschafer@cs.washington.edu>
2017-05-12 17:25:59 -0700

    Add rudimentary style and labels

--- docs/prototype/js/ethnicty.js
+++ docs/prototype/js/ethnicty.js
@@ -1,21 +1,25 @@
 const d3 = require('d3');
+const d3Chromatic = require('d3-scale-chromatic');
 
 (function() {
     var w = $(window).width() * 0.5;
     var h = $(window).height() * 0.5;
-    var radius = Math.min(w, h) / 2;
+    var radius = Math.min(w, h) / 3;
     var svg = null;
 
-    var color = d3.scaleOrdinal(d3.schemeCategory10);
+    var color = d3.scaleOrdinal(d3Chromatic.schemeDark2);
     var pie = d3.pie()
-        .sort(null)
+        .sort(compareStrings)
         .value(function(d) { return d.value; });
     var path = d3.arc()
         .outerRadius(radius - 10)
         .innerRadius(0);
+    var outerArc = d3.arc()
+	    .innerRadius(radius * 0.9)
+	    .outerRadius(radius * 0.9);
     var label = d3.arc()
-        .outerRadius(radius - 40)
-        .innerRadius(radius - 40);
+        .outerRadius(radius + 20)
+        .innerRadius(radius + 10);
 
 
     $(document).ready(function() {
@@ -31,6 +35,11 @@ const d3 = require('d3');
             .attr("width", w)
             .attr("height", h)
             .attr("class", "pieSVG");
+        svg.append("rect")
+            .attr("width", w)
+            .attr("height", h)
+            .attr("class", "backgroundRect");
+
 
         svg.append("g")
             .attr("transform", "translate(" + w / 4 + "," + h / 2 + ")")
@@ -58,26 +67,31 @@ const d3 = require('d3');
         var censusPie = svg.select("#censusPie");
         drawPie(victimPie, ethnicities);
         drawPie(censusPie, censusData);
-
     }
 
     function drawPie(g, data) {
-        var sorted = data.sort(function(d1, d2) {
-            if (d1.key < d2.key) {
-                return -1;
-            } else if (d1.key > d2.key) {
-                return 1;
-            } else {
-                return 0;
-            }
-        });
+        var sorted = data.sort(compareStrings);
         console.log(sorted);
         var arc = g.selectAll(".arc")
             .data(pie(sorted))
             .enter().append("g")
-                .attr("class", "arc");
+            .attr("class", "arc");
         arc.append("path")
             .attr("d", path)
             .attr("fill", function(_, i) {return color(i); });
+        arc.append("text")
+            .attr("transform", function(d) { return "translate(" + label.centroid(d) + ")"; })
+            .attr("dy", "0.35em")
+            .text(function(d) { return d.data.key; });
+    }
+
+    function compareStrings(s1, s2) {
+        if (s1.key < s2.key) {
+            return 1;
+        } else if (s2.key === s1.key) {
+            return 0;
+        } else {
+            return -1;
+        }
     }
 }());
