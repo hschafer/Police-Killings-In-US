226999ccd9632225c0bb9635b26672e62ee1d719
Hunter Schafer <hschafer@cs.washington.edu>
2017-05-17 01:22:01 -0700

    Little bit of code clean up

--- docs/prototype/js/ethnicty.js
+++ docs/prototype/js/ethnicty.js
@@ -2,179 +2,12 @@ const d3 = require('d3');
 const d3Chromatic = require('d3-scale-chromatic');
 const Chart = require('chart.js');
 require('waypoints/lib/jquery.waypoints.js');
+require('./chart-extensions.js')
 
 var OUTLINE_COLOR = "#b1b1b1";
 var BACKGROUND_COLOR = "#2f394d";
 
 Chart.defaults.global.defaultFontColor = OUTLINE_COLOR;
-
-var baseController = Chart.controllers.doughnut;
-Chart.defaults.nestedDoughnut = Chart.helpers.extend(Chart.defaults.doughnut, {
-    rotations: [Chart.defaults.doughnut.rotation, Chart.defaults.doughnut.rotation]
-});
-
-Chart.controllers.nestedDoughnut = Chart.controllers.doughnut.extend({
-    // The rest of this code is copied from https://github.com/chartjs/Chart.js/blob/master/src/controllers/controller.doughnut.js
-    // so that we can extend it to allow each nested chart to have different rotations
-    update: function(reset) {
-        var me = this;
-        var chart = me.chart,
-            chartArea = chart.chartArea,
-            opts = chart.options,
-            arcOpts = opts.elements.arc,
-            availableWidth = chartArea.right - chartArea.left - arcOpts.borderWidth,
-            availableHeight = chartArea.bottom - chartArea.top - arcOpts.borderWidth,
-            minSize = Math.min(availableWidth, availableHeight),
-            offset = {
-                x: 0,
-                y: 0
-            },
-            meta = me.getMeta(),
-            cutoutPercentage = opts.cutoutPercentage,
-            circumference = opts.circumference,
-            ringIndex = me.getRingIndex(me.index);
-
-        // If the chart's circumference isn't a full circle, calculate minSize as a ratio of the width/height of the arc
-        if (circumference < Math.PI * 2.0) {
-            var startAngle = opts.rotations[ringIndex] % (Math.PI * 2.0);
-            startAngle += Math.PI * 2.0 * (startAngle >= Math.PI ? -1 : startAngle < -Math.PI ? 1 : 0);
-            var endAngle = startAngle + circumference;
-            var start = {x: Math.cos(startAngle), y: Math.sin(startAngle)};
-            var end = {x: Math.cos(endAngle), y: Math.sin(endAngle)};
-            var contains0 = (startAngle <= 0 && 0 <= endAngle) || (startAngle <= Math.PI * 2.0 && Math.PI * 2.0 <= endAngle);
-            var contains90 = (startAngle <= Math.PI * 0.5 && Math.PI * 0.5 <= endAngle) || (startAngle <= Math.PI * 2.5 && Math.PI * 2.5 <= endAngle);
-            var contains180 = (startAngle <= -Math.PI && -Math.PI <= endAngle) || (startAngle <= Math.PI && Math.PI <= endAngle);
-            var contains270 = (startAngle <= -Math.PI * 0.5 && -Math.PI * 0.5 <= endAngle) || (startAngle <= Math.PI * 1.5 && Math.PI * 1.5 <= endAngle);
-            var cutout = cutoutPercentage / 100.0;
-            var min = {x: contains180 ? -1 : Math.min(start.x * (start.x < 0 ? 1 : cutout), end.x * (end.x < 0 ? 1 : cutout)), y: contains270 ? -1 : Math.min(start.y * (start.y < 0 ? 1 : cutout), end.y * (end.y < 0 ? 1 : cutout))};
-            var max = {x: contains0 ? 1 : Math.max(start.x * (start.x > 0 ? 1 : cutout), end.x * (end.x > 0 ? 1 : cutout)), y: contains90 ? 1 : Math.max(start.y * (start.y > 0 ? 1 : cutout), end.y * (end.y > 0 ? 1 : cutout))};
-            var size = {width: (max.x - min.x) * 0.5, height: (max.y - min.y) * 0.5};
-            minSize = Math.min(availableWidth / size.width, availableHeight / size.height);
-            offset = {x: (max.x + min.x) * -0.5, y: (max.y + min.y) * -0.5};
-        }
-
-        chart.borderWidth = me.getMaxBorderWidth(meta.data);
-        chart.outerRadius = Math.max((minSize - chart.borderWidth) / 2, 0);
-        chart.innerRadius = Math.max(cutoutPercentage ? (chart.outerRadius / 100) * (cutoutPercentage) : 0, 0);
-        chart.radiusLength = (chart.outerRadius - chart.innerRadius) / chart.getVisibleDatasetCount();
-        chart.offsetX = offset.x * chart.outerRadius;
-        chart.offsetY = offset.y * chart.outerRadius;
-
-        meta.total = me.calculateTotal();
-
-        me.outerRadius = chart.outerRadius - (chart.radiusLength * ringIndex);
-        me.innerRadius = Math.max(me.outerRadius - chart.radiusLength, 0);
-
-        Chart.helpers.each(meta.data, function(arc, index) {
-            me.updateElement(arc, index, reset, ringIndex);
-        });
-    },
-
-    updateElement: function(arc, index, reset, ringIndex) {
-        var me = this;
-        console.log("Updating element", me);
-        var chart = me.chart,
-            chartArea = chart.chartArea,
-            opts = chart.options,
-            animationOpts = opts.animation,
-            centerX = (chartArea.left + chartArea.right) / 2,
-            centerY = (chartArea.top + chartArea.bottom) / 2,
-            startAngle = opts.rotations[index], // non reset case handled later
-            endAngle = opts.rotations[index], // non reset case handled later
-            //startAngle = opts.rotation,
-            //endAngle = opts.rotation,
-            dataset = me.getDataset(),
-            circumference = reset && animationOpts.animateRotate ? 0 : arc.hidden ? 0 : me.calculateCircumference(dataset.data[index]) * (opts.circumference / (2.0 * Math.PI)),
-            innerRadius = reset && animationOpts.animateScale ? 0 : me.innerRadius,
-            outerRadius = reset && animationOpts.animateScale ? 0 : me.outerRadius,
-            valueAtIndexOrDefault = Chart.helpers.getValueAtIndexOrDefault;
-
-        Chart.helpers.extend(arc, {
-            // Utility
-            _datasetIndex: me.index,
-            _index: index,
-
-            // Desired view properties
-            _model: {
-                x: centerX + chart.offsetX,
-                y: centerY + chart.offsetY,
-                startAngle: startAngle,
-                endAngle: endAngle,
-                circumference: circumference,
-                outerRadius: outerRadius,
-                innerRadius: innerRadius,
-                label: valueAtIndexOrDefault(dataset.label, index, chart.data.labels[index])
-            }
-        });
-
-        var model = arc._model;
-        // Resets the visual styles
-        this.removeHoverStyle(arc);
-
-        // Set correct angles if not resetting
-        if (!reset || !animationOpts.animateRotate) {
-            if (index === 0) {
-                model.startAngle = opts.rotations[ringIndex];
-            } else {
-                model.startAngle = me.getMeta().data[index - 1]._model.endAngle;
-            }
-
-            model.endAngle = model.startAngle + model.circumference;
-        }
-
-        arc.pivot();
-    }
-});
-
-console.log(Chart.controllers.nestedDoughnut);
-
-Chart.pluginService.register({
-    beforeDraw: function (chart) {
-        if (chart.config.options.elements.center) {
-            //Get ctx from string
-            var ctx = chart.chart.ctx;
-
-            //Get options from the center object in options
-            var centerConfig = chart.config.options.elements.center;
-            var fontStyle = centerConfig.fontStyle || 'Arial';
-            var txt = centerConfig.text;
-            var color = centerConfig.color || '#000';
-            var sidePadding = centerConfig.sidePadding || 20;
-            var sidePaddingCalculated = (sidePadding/100) * (chart.innerRadius * 2)
-
-            //Start with a base font of 30px
-            ctx.font = "30px " + fontStyle;
-            //Get the width of the string and also the width of the element minus 10 to give it 5px side padding
-            var stringWidth = ctx.measureText(txt).width;
-            var elementWidth = (chart.innerRadius * 2) - sidePaddingCalculated;
-
-            // Find out how much the font can grow in width.
-            var widthRatio = elementWidth / stringWidth;
-            var newFontSize = Math.floor(30 * widthRatio);
-            var elementHeight = (chart.innerRadius * 2);
-
-            // Pick a new font size so it will not be larger than the height of label.
-            var fontSizeToUse = Math.min(newFontSize, elementHeight);
-
-            //Set font settings to draw it correctly.
-            var centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
-            var centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2);
-            ctx.textAlign = 'center';
-            ctx.textBaseline = 'middle';
-            ctx.font = fontSizeToUse+"px " + fontStyle;
-            ctx.fillStyle = color;
-            ctx.fillText(txt, centerX, centerY);
-
-            // If we have a config for the right, use the same stuff as the center except for the text
-            if (chart.config.options.elements.right) {
-                var rightTxt = chart.config.options.elements.right.text;
-                var rightStringWidth = ctx.measureText(rightTxt).width;
-                var rightX = centerX + chart.outerRadius + rightStringWidth / 2 + sidePaddingCalculated / 4;
-                ctx.fillText(rightTxt, rightX, centerY);
-            }
-        }
-    }
-});
 
 function tooltipLabel(tooltipItem, data, signed) {
     var val = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
@@ -184,7 +17,6 @@ function tooltipLabel(tooltipItem, data, signed) {
     }
     return data.labels[tooltipItem.index] + ": " + formattedVal + "%";
 }
-
 
 (function() {
     var w = $(window).width() * 0.55;
@@ -228,7 +60,6 @@ function tooltipLabel(tooltipItem, data, signed) {
         if (error) throw error;
         victimData = victimData.sort(compareStrings);
         censusData = censusData.sort(compareStrings);
-
         var colors = victimData.map(function(_, i) { return color(i); });
 
         var pieCharts = makePieCharts(victimData, colors);
@@ -264,7 +95,6 @@ function tooltipLabel(tooltipItem, data, signed) {
             },
             options: {
                 rotationIndex: 0,
-                rotations: [-0.5 * Math.PI, -0.5 * Math.PI],
                 elements: {
 				    center: {
 					    text: "Race of Victims",
@@ -279,17 +109,32 @@ function tooltipLabel(tooltipItem, data, signed) {
                     }
                 },
                 legend: {
-                    labels: {
-                        onClick: rotateChart
-                    }
+                    onClick: rotateChart
                 },
                 onClick: rotateChart
 			}
         });
     }
 
+    function animatePieChart(chart, censusData) {
+        var config = chart.config;
+        config.data.datasets.unshift({
+            label: 'Percentage of Population',
+            data: censusData.map(function(d) { return d.value;}),
+            backgroundColor: config.data.datasets[0].backgroundColor, // use the same color
+            borderColor: BACKGROUND_COLOR,
+            borderWidth: 2
+        });
+        config.options.elements.right = {
+            text: "Race of Population",
+        }
+        chart.update();
+        setTimeout(function() { $("#diffChart").fadeIn("slow"); }, 800);
+        setTimeout(function() { $("#textReveal").fadeIn("slow"); }, 800);
+    }
+
     function rotateChart(event, clicked) {
-        if (clicked) {
+        if (clicked.length > 0) {
             var clickedElem = clicked[0];
             var index = clickedElem._index;
             var chart = clickedElem._chart;
@@ -299,7 +144,6 @@ function tooltipLabel(tooltipItem, data, signed) {
             for (var i = 0; i < datasets.length; i++) {
                 var data = datasets[i].data;
                 var sum = 0;
-                console.log(data);
                 if (index < rotationIndex) {
                     for (var j = index; j < rotationIndex; j++) {
                         sum += data[j];
@@ -353,23 +197,6 @@ function tooltipLabel(tooltipItem, data, signed) {
                 }
             }
         })
-    }
-
-    function animatePieChart(chart, censusData) {
-        var config = chart.config;
-        config.data.datasets.unshift({
-            label: 'Percentage of Population',
-            data: censusData.map(function(d) { return d.value;}),
-            backgroundColor: config.data.datasets[0].backgroundColor, // use the same color
-            borderColor: BACKGROUND_COLOR,
-            borderWidth: 2
-        });
-        config.options.elements.right = {
-            text: "Race of Population",
-        }
-        chart.update();
-        setTimeout(function() { $("#diffChart").fadeIn("slow"); }, 800);
-        setTimeout(function() { $("#textReveal").fadeIn("slow"); }, 800);
     }
 
     function compareStrings(s1, s2) {
