e4e02bd08f6cc112a91611664db62c8084cf81f3
Hunter Schafer <hschafer@uw.edu>
2017-05-26 12:42:29 -0700

    Make bubble move when transitioning

--- docs/prototype/js/bundle.js
+++ docs/prototype/js/bundle.js
@@ -34382,20 +34382,28 @@ const fuse = __webpack_require__(239);
             var randSymbol = visibleCitySymbols[Math.floor(Math.random() * visibleCitySymbols.length)];
             var randCity = randSymbol.__data__;
 
-            deselectCity();
-            selectCity(randCity);
-
             // Note: This is a different highlighting strategy than clicking.
             // We want this bubble to be on the top so it's easier to just make a duplicate
-            var circle = svg.selectAll("#highlightedCityDuplicate").data([randCity], function(d) { return d.id; });
+            var circle = svg.selectAll("#highlightedCityDuplicate").data([randCity]);
             circle.enter().append('circle')
                 .attr("id", "highlightedCityDuplicate")
                 .attr("class", "symbol")
                 .merge(circle)
+                .transition().duration(1500)
                 .attr("cx", randSymbol.cx.animVal.value)
                 .attr("cy", randSymbol.cy.animVal.value)
                 .attr("r", randSymbol.r.animVal.value);
             circle.exit().remove();
+
+            // Important, don't highlight it now on the map because then there will be
+            // two dots, but do change the content on the right.
+            deselectCity();
+            selectCity(randCity, false);
+            setTimeout(function() {
+                d3.selectAll(".symbol")
+                    .filter(function(d) { return d === city; })
+                    .classed("highlightedCity", true);
+            }, 1500);
         }
     }
 
@@ -34407,7 +34415,9 @@ const fuse = __webpack_require__(239);
         setVictimCount(stateVictimCount.get(d.properties.abbreviation));
     }
 
-    function selectCity(city) {
+    function selectCity(city, highlightOnMap) {
+        highlightOnMap = typeof(highlightOnMap) === "undefined" ? true : highlightOnMap; // default true
+
         $("#cityNameAndSearch").val(city.city + ", " + city.state);
 
         // add victims
@@ -34419,9 +34429,11 @@ const fuse = __webpack_require__(239);
         setVictimCount(city.records_visible.length);
 
         // highlight the city
-        d3.selectAll(".symbol")
-            .filter(function(d) { return d === city; })
-            .classed("highlightedCity", true);
+        if (highlightOnMap) {
+            d3.selectAll(".symbol")
+                .filter(function(d) { return d === city; })
+                .classed("highlightedCity", true);
+        }
     }
 
     function deselectCity() {
