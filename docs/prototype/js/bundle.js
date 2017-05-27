2b23c3a6f6940f856ca6324c1af23f0abcc7ee47
Hunter Schafer <hschafer@uw.edu>
2017-05-27 16:39:04 -0700

    Make clicking references jump to end

--- docs/prototype/js/bundle.js
+++ docs/prototype/js/bundle.js
@@ -35432,21 +35432,21 @@ Chart.pluginService.register({
 // Our modules are designed to execute when imported
 __webpack_require__(136);
 __webpack_require__(138);
-const fbiComparison = __webpack_require__(137);
+__webpack_require__(137);
 
 __webpack_require__(139);
 
 $(document).ready(function() {
-    var drawPeople = fbiComparison.drawPeople;
-    var peopleLoaded = false;
-
+    // set up full page
     $('#fullpage').fullpage({
         autoScrolling: false,
         fitToSection: false,
     });
 
+    // slow highlight of intro quote
     setTimeout(highlightIntro, 1500);
 
+    // next steps tabs
     $("ul.tabs li").click(function() {
         var me = $(this);
         var tabbed = me.attr("data-tab");
@@ -35456,6 +35456,13 @@ $(document).ready(function() {
 
 		me.addClass('current');
 		$('#' + tabbed).addClass('current');
+    });
+
+    // make citations clickable
+    $(".citation").click(function() {
+        // this is the location of the references slide.
+        // Please change if you add a new slide
+        $.fn.fullpage.moveTo(8);
     });
 });
 
