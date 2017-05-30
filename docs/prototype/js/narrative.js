ce52bb5640783708b485f685a773465921ca4694
Hunter Schafer <hschafer@uw.edu>
2017-05-30 15:43:13 -0700

    Put text in one section

--- docs/prototype/js/narrative.js
+++ docs/prototype/js/narrative.js
@@ -6,67 +6,55 @@ require('waypoints/lib/jquery.waypoints.js');
 
 (function () {
 
-    var narrative1_text = "One summer afternoon in Seattle, Washington " +
+    var narrativeText = [
+        "One summer afternoon in Seattle, Washington " +
         "in 2010, Officer Ian Birk noticed a man crossing a city street " +
         "holding a knife. Officer Birk, a member of the Seattle Police " +
-        "Department, left his vehicle to handle the situation. " ;
+        "Department, left his vehicle to handle the situation. ",
 
-    var narrative2_text = "Officer Birk approached the man, commanding " +
+        "Officer Birk approached the man, commanding " +
         "him to put down the knife. 4 seconds later, Officer Birk had " +
-        "fired four shots into the man, killing him.";
+        "fired four shots into the man, killing him.",
 
-    var narrative3_text = "The man, John T. Williams, was a member of " +
+        "The man, John T. Williams, was a member of " +
         "the Nuu-chah-nulth tribal group native to nearby Vancouver Island. " +
         "He was a local woodcarver, hard of hearing, and was carrying a block of " +
-        "wood and his carving knife.";
+        "wood and his carving knife."
+    ];
+
+    var duration = 4000;
 
     $(document).ready(function () {
         setTimeout(function () {
                 var waypoint1 = new Waypoint({
-                    element: $("#narrative_section1"),
+                    element: $(".narrative_section"),
                     handler: function () {
-                        typeInText("#narrative_section1 .narrative_text p", narrative1_text);
+                        typeInText(0);
                         waypoint1.disable();
                     },
                     offset: 300
                 });
-
-                var waypoint2 = new Waypoint({
-                    element: $("#narrative_section2"),
-                    handler: function () {
-                        typeInText("#narrative_section2 .narrative_text p", narrative2_text);
-                        waypoint2.disable();
-                    },
-                    offset: 300
-                });
-
-                var waypoint3 = new Waypoint({
-                    element: $("#narrative_section3"),
-                    handler: function () {
-                        typeInText("#narrative_section3 .narrative_text p", narrative3_text);
-                        waypoint3.disable();
-                    },
-                    offset: 300
-                });
-
-            }, 1000
-        );
+            }, 1000);
     });
 
-    function typeInText(id_selector, text) {
-        console.log("type text from" + id_selector + " now");
-        var p = d3.select(id_selector);
-        var textLength = text.length;
-        p.transition()
-            .duration(4000)
-            .ease(d3.easeLinear)
-            .tween('text', function() {
-                return function(t) {
-                    var newText = text.substr(0,
-                            Math.round( t * textLength));
-                    d3.select(id_selector).html(newText);
-                };
-            });
+    function typeInText(index) {
+        if (index < narrativeText.length) {
+            console.log("type text from " + index + " now");
+            var p = d3.select("#narrative_text" + index + " p");
+            var text = narrativeText[index];
+            var textLength = text.length;
+            p.transition()
+                .duration(duration)
+                .ease(d3.easeLinear)
+                .tween('text', function() {
+                    return function(t) {
+                        var newText = text.substr(0,
+                                Math.round( t * textLength));
+                        p.html(newText);
+                    };
+                });
+            setTimeout(function() { typeInText(index + 1); }, duration * 1.1);
+        }
             //.tween("text", function () {
             //    //debugger;
             //    //var textcontent = this.textContent;
